a89279548ca9d9db6a9d2e9eb163c4b7d395033b
Victor Yang <victor.yang@hellosanta.com.tw>
2020-01-04 01:37:35 +0800

    TEST REsult

--- post.sh
+++ post.sh
@@ -4,5 +4,5 @@ curl \
   -X "POST" \
   -H "Content-Type: application/json" \
   -H "Authorization: Bearer ${PLUGIN_TOKEN}" \
-  -d '{"channel_id": "'${PLUGIN_CHANNEL_ID}'", "message":"'${DRONE_BUILD_STATUS}'"}' \
+  -d '{"channel_id": "'${PLUGIN_CHANNEL_ID}'", "message":"`${DRONE_BUILD_STATUS}`"}' \
   "${PLUGIN_URL}/api/v4/posts"
