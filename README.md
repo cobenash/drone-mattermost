1da587e5b33d864ac63d35f42a72685bc19d0ed6
Victor Yang <victor.yang@hellosanta.com.tw>
2020-01-04 20:09:13 +0800

    String replace

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 A plugin which can provide mattermost notify
 
 ## How to use drone Mattermost
-With this plugin, we can decide whether we can send message to channel or send direct message to the user.
+With this plugin, we can decide whether we can send message to channel.
 
 Therefore, we need to create bot account first and use the bot account id as the main input.
 
@@ -26,34 +26,10 @@ pipeline:
 $ docker run --rm \
 -e PLUGIN_TOKEN=o7kkk5qszffrjfz3ygjn8eqxse \
 -e PLUGIN_CHANNEL_ID=63i6qpnakj8e5yqokeaz6tg9aa \
--e PLUGIN_BOT_ID=bot-master \
 -e PLUGIN_URL=https://chat.hellosanta.tw \
 -e PLUGIN_TO=victor \
--e PLUGIN_SEND_TO_CHANNEL=true \
--e DRONE_REPO_OWNER=hellosanta \
--e DRONE_REPO_NAME=sampel-project \
--e DRONE_COMMIT_SHA=45da7659390f729d97068701718adc21d81c0fd5 \
--e DRONE_COMMIT_BRANCH=master \
--e DRONE_COMMIT_AUTHOR=victor.yang \
--e DRONE_COMMIT_AUTHOR_EMAIL=victor.yang@hellosanta.com.tw \
--e DRONE_COMMIT_MESSAGE=update \
--e DRONE_BUILD_NUMBER=1 \
--e DRONE_BUILD_STATUS=success \
--e DRONE_BUILD_LINK=http://github.com/hellosanta/sample-project \
--e DRONE_JOB_STARTED=1477550560 \
--e DRONE_JOB_FINISHED=1477550580 \
-plugin/mattermost
-```
-
-## Testing for sending msg directly
-```shell=
-$ docker run --rm \
--e PLUGIN_TOKEN=o7kkk5qszffrjfz3ygjn8eqxse \
--e PLUGIN_CHANNEL_ID=63i6qpnakj8e5yqokeaz6tg9aa \
--e PLUGIN_BOT_ID=y4u17ro4p78f5x6xkra7y4fdta \
--e PLUGIN_URL=https://chat.hellosanta.tw \
--e PLUGIN_TO=bhwy6ttagtggbqfneannk4k95a \
--e PLUGIN_SEND_TO_CHANNEL=false \
+-e PLUGIN_ONLY_MATCH_EMAIL=true \
+-e PLUGIN_MESSAGE="專案:[build.repo_name] 執行編號:[build.number]  Msg:[build.commit_message]  建立狀態:[build.status]  查看連結:[build.link]" \
 -e DRONE_REPO_OWNER=hellosanta \
 -e DRONE_REPO_NAME=sampel-project \
 -e DRONE_COMMIT_SHA=45da7659390f729d97068701718adc21d81c0fd5 \
--- post.sh
+++ post.sh
@@ -8,9 +8,9 @@ branch=$DRONE_COMMIT_BRANCH
 author=$DRONE_COMMIT_AUTHOR
 author_email=$DRONE_COMMIT_AUTHOR_EMAIL
 commit_message=$DRONE_COMMIT_MESSAGE
-build_number=$DRONE_BUILD_NUMBER
+number=$DRONE_BUILD_NUMBER
 status=$DRONE_BUILD_STATUS
-build_link=$DRONE_BUILD_LINK
+link=$DRONE_BUILD_LINK
 job_started=$DRONE_JOB_STARTED
 job_finished=$DRONE_JOB_FINISHED
 # End of drone parameter
@@ -22,56 +22,32 @@ channel_id=$PLUGIN_CHANNEL_ID
 bot_id=$PLUGIN_BOT_ID
 to=$PLUGIN_TO
 only_match_email=$PLUGIN_ONLY_MATCH_EMAIL
-send_to_channel=$PLUGIN_SEND_TO_CHANNEL
 message=$PLUGIN_MESSAGE
 
-## API Type
-## If the send_to_channel is direct, we must provide the PLUGIN_TO parameter, or it will throw the error.
-if $send_to_channel ; then
-  apiURL="$url/api/v4/posts";
-  ## Establish Sending Message send to channel.
-  full_message='{"channel_id": "'${channel_id}'", "message":"'${DRONE_BUILD_STATUS}'"}';
-  ## 傳送資料
-  curl -i \
+## replace string 
+message="${message//\[build.owner\]/$owner}"
+message="${message//\[build.repo_name\]/$repo_name}"
+message="${message//\[build.commit_sha\]/$commit_sha}"
+message="${message//\[build.branch\]/$branch}"
+message="${message//\[build.author_email\]/$author_email}"
+message="${message//\[build.commit_message\]/$commit_message}"
+message="${message//\[build.status\]/$status}"
+message="${message//\[build.link\]/$link}"
+message="${message//\[build.job_started\]/$job_started}"
+message="${message//\[build.job_finished\]/$job_finished}"
+message="${message//\[build.number\]/$number}"
+
+
+# Send Message
+apiURL="$url/api/v4/posts";
+
+full_message=$( jq -n \
+  --arg channel_id "$channel_id" \
+  --arg message "$message" \
+  '{channel_id: $channel_id,message: $message}')
+curl -i \
     -X "POST" \
     -H "Content-Type: application/json" \
     -H "Authorization: Bearer ${token}" \
     -d "${full_message}" \
     $apiURL
-
-else
-  apiURL="$url/api/v4/channels/direct";
-  Direct_Array=$( jq -n \
-  --arg bot "$bot_id" \
-  --arg user "$to" \
-  '{$bot,$user}')
-  echo $DirectArray
-  ## 傳送資料
-  # curl -i \
-  #   -X "POST" \
-  #   -H "Content-Type: application/json" \
-  #   -H "Authorization: Bearer ${token}" \
-  #   -d "${DirectArray}" \
-  #   $apiURL
-  # rtnFromMM=$(curl -i -X POST -H "Content-Type: application/json" -H "Authorization: Bearer $token" -d $DirectArray $apiURL)
-  # echo $rtnFromMM
-fi
-
-
-
-
-BUCKET_NAME=testbucket
-OBJECT_NAME=testworkflow-2.0.1.jar
-TARGET_LOCATION=/opt/test/testworkflow-2.0.1.jar
-
-JSON_STRING=$( jq -n \
-                  --arg bn "$BUCKET_NAME" \
-                  --arg on "$OBJECT_NAME" \
-                  --arg tl "$TARGET_LOCATION" \
-                  '{bucketname: $bn, objectname: $on, targetlocation: $tl}' )
-
-
-
-echo $JSON_STRING
-# echo $full_message;
-# echo $apiURL;
